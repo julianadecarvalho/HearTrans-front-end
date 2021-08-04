9990a6808d3507eb703b1289958c8332b2568d9d
Hannah Lumapas <lumapas.h@gmail.com>
2021-08-03 20:44:07 -0400

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { Home } from "./components/Home";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { SearchResults } from "./components/SearchResults";
 
 function App() {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route path="/search" component={SearchResults} />
+        <Route path="/" component={Home} />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
---
+++ src/components/SearchResults.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { NavBar } from "./NavBar";
+
+export function SearchResults() {
+  return (
+    <NavBar />
+    // add search summary results
+    // cards component list
+    // map component
+  );
+}
