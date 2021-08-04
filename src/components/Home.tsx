f32777ee407a5c96d11145cfeba1d13ed250a0e3
Drew Taylor <drewlynntaylor@gmail.com>
2021-08-03 20:16:21 -0700

    Still some errors but most of it is done

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import { NavBar } from "./NavBar";
+
 
 export function Home() {
   return (
     <div>
-      <NavBar />
       <section>
         <h1>About Us</h1>
       </section>
---
+++ src/components/Menu.tsx
@@ -0,0 +1,13 @@
+import React, { useState} from "react";
+import * as FaIcons from 'react-icons/fa';
+import * as AiIcons from 'react-icons/ai';
+import * as FcIcons from 'react-icons/fc';
+
+export const Menu = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: FcIcons.FcHome,
+        name: 'menu-text',
+    },
+]
