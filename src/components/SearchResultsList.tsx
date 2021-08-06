cd82a319228f081f26e0bbdd351fa184e59dd848
Hannah Lumapas <lumapas.h@gmail.com>
2021-08-05 21:00:29 -0400

    render results information on search results page

--- src/components/SearchResultsList.tsx
+++ src/components/SearchResultsList.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import { SearchResultCard } from "./SearchResultCard";
 import styles from "./SearchResultsList.module.css";
+import { Info } from "../pages/SearchResults";
 
-export function SearchResultsList() {
-  return (
-    <div className={styles["search-results-list"]}>
-      <SearchResultCard />
-      <SearchResultCard />
-    </div>
-  );
+interface IProps {
+  results: Info[];
 }
+
+export const SearchResultsList: React.FC<IProps> = ({ results }) => {
+  const renderList = (): JSX.Element[] => {
+    return results.map((result) => {
+      return (
+        <li className="searchResultsList">
+          <SearchResultCard
+            name={result.name}
+            avgReview={result.avgReview}
+            specialty={result.specialty}
+            id={result.id}
+          />
+        </li>
+      );
+    });
+  };
+
+  return (
+    <article className={styles["search-results-list"]}>
+      <ul className={styles["search-results-list"]}>{renderList()}</ul>
+    </article>
+  );
+};
+
+export default SearchResultsList;
--- src/pages/SearchResults.tsx
+++ src/pages/SearchResults.tsx
@@ -1,14 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavBar } from "../components/NavBar";
 import { SearchResultsList } from "../components/SearchResultsList";
 
+export interface Info {
+  name: string;
+  avgReview: string;
+  specialty: string;
+  id: number;
+}
+
+// export interface IState {
+//   resultsInfo: {
+//     name: string;
+//     avgReview: string;
+//     specialty: string;
+//     id: number;
+//   }[];
+// }
+
 export function SearchResults() {
+  const [results, setResults] = useState<Info[]>([
+    {
+      name: "Dr. Drew",
+      avgReview: "5",
+      specialty: "psychiatry",
+      id: 1,
+    },
+    {
+      name: "Dr. Hannah",
+      avgReview: "5",
+      specialty: "gastrointerology",
+      id: 2,
+    },
+    {
+      name: "Baker's Clinic",
+      avgReview: "5",
+      specialty: "vet",
+      id: 3,
+    },
+  ]);
+
   return (
     <div>
       <NavBar />
       {/* add search summary results component */}
       {/* cards component list */}
-      <SearchResultsList />
+      <SearchResultsList results={results} />
       {/* map component */}
     </div>
   );
