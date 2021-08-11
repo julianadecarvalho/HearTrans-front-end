0f7c362ab963b5816a3012cc1113eadde6b935eb
Juliana DeCarvalho Sutherland <julianadsutherland@gmail.com>
2021-08-11 13:21:49 -0700

    merging

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -10,7 +10,7 @@ import GoogleMapReact from 'google-map-react';
 // }
 // const AnyReactComponent: React.FC = ({ text }) => <div>{ text }</div>;
 
-const AnyReactComponent = (props: {text: string}) =>  (
+const AnyReactComponent = (props: {text: string, lat: number, lng: number}) =>  (
     <div>{props.text}</div>
 )
 
@@ -35,8 +35,8 @@ class SimpleMap extends Component {
           yesIWantToUseGoogleMapApiInternals
         >
           <AnyReactComponent
-            // lat={59.955413}
-            // lng={30.337844}
+            lat={59.955413}
+            lng={30.337844}
             text="My Marker"
           />
         </GoogleMapReact>
