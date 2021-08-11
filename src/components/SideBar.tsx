daf29f3352c8a9c574d779de709a943d4501cb74
Drew Taylor <drewlynntaylor@gmail.com>
2021-08-11 13:14:21 -0700

    working on css

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -3,8 +3,9 @@ import * as FaIcons from "react-icons/fa";
 import * as AiIcons from "react-icons/ai";
 import { Menu } from "./Menu";
 import { Link } from "react-router-dom";
-import "./SideBar.css";
+import styles from "./SideBar.module.css";
 import { IconContext } from "react-icons";
+
 
 export function SideBar() {
     const [sidebar, setSidebar] = useState(false);
@@ -12,14 +13,14 @@ export function SideBar() {
     return (
         <>
         <IconContext.Provider value={{ color: 'light blue'}}>
-        <div className="navbar">
-            <Link to="#" className="menu-bars">
+        <div className={styles["navbar"]}>
+            <Link to="#" className={styles["menu-bars"]}>
             <FaIcons.FaBars onClick={showSidebar} />
             </Link>
-            <nav className={sidebar ? "nav-menu active" : "nav-menu"}>
-            <ul className="nav-menu-items" onClick={showSidebar}>
-                <li className="navbar-toggle">
-                <Link to="#" className="menu-bars">
+            <nav className={sidebar ? styles["nav-menu active"] : styles["nav-menu"]}>
+            <ul className={styles["nav-menu-items"]} onClick={showSidebar}>
+                <li className={styles["navbar-toggle"]}>
+                <Link to="#" className={styles["menu-bars"]}>
                     <AiIcons.AiOutlineClose />
                 </Link>
                 </li>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,6 +5,16 @@ export function Home() {
     <div>
       <section>
         <h1>About Us</h1>
+        <p>Hi!!</p>
+        <p>Welcome to our website! We are programming students at The Ada Developers Academy and this is our capstone project.</p>
+      </section>
+      <section>
+        <h1>Mission</h1>
+        <p>Our mission is to help transgender people find medical providers and locations that will help them. We wanted to make sure that people can leave reviews so that in case those locations and providers actually respect people's preferences then other transgender people will know they can go to them.</p>
+      </section>
+      <section>
+        <h1>Important!</h1>
+        <p>We don't collect user data so there is no issues about it being leaked.</p>
       </section>
     </div>
   );
