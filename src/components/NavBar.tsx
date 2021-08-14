07af4e57bf37c9a3660fd9ac9664edca68f65151
Juliana DeCarvalho Sutherland <julianadsutherland@gmail.com>
2021-08-14 10:43:12 -0700

    image success!

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,13 +2,14 @@ import React, { useState } from "react";
 import styles from "./NavBar.module.css";
 import { SearchBar } from "./SearchBar";
 import { SideBar } from "./SideBar";
+import logo from '../images/heartranslogo.png'; 
 
 export function NavBar() {
   return (
     <div className={styles["nav-bar"]}>
       <div className="navbar-brand">
         <a className="navbar-item" href="/">
-          <span>Logo here</span>
+          <img src= {logo} alt="heartrans-logo"/>
         </a>
       </div>
       <section className="navbar-menu">
