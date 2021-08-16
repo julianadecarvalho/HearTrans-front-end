32e2f20abe1377f2097d55a2c05f50b1c86ec3c7
Hannah Lumapas <lumapas.h@gmail.com>
2021-08-16 16:23:50 -0400

    changing config variables

--- seed/seed.py
+++ seed/seed.py
@@ -1,18 +1,47 @@
+# # import requests
+# # import json
+# # url = "http://heartrans-back.herokuapp.com/providers/"
+# # payload = json.dumps({
+# #     "fullName": "Sydney Woolston",
+# #     "otherNames": [
+# #         "Sydney Woolston"
+# #     ],
+# #     "titles": [
+# #         "License Mental Health Counselor Associate"
+# #     ],
+# #     "specialties": ["Psychology"],
+# #     "services": [
+# #         "Couples/Relationship therapy",
+# #         "Individual therapy"
+# #     ],
+# #     "languages": ["English"],
+# #     "remoteVisits": True,
+# #     "slidingScalePay": False
+# # })
+# # headers = {
+# #     'Content-Type': 'application/json'
+# # }
+# # response = requests.request("POST", url, headers=headers, data=payload)
+# # print(response.text)
+
 # import requests
 # import json
+
+# # provider request bodies 
 # url = "http://heartrans-back.herokuapp.com/providers/"
 # payload = json.dumps({
-#     "fullName": "Sydney Woolston",
+#     "fullName": "Chester Robachinski",
 #     "otherNames": [
-#         "Sydney Woolston"
+#         "Chet Robachinski"
 #     ],
 #     "titles": [
-#         "License Mental Health Counselor Associate"
+#         "MD"
 #     ],
-#     "specialties": ["Psychology"],
+#     "specialties": "Psychiatry",
 #     "services": [
 #         "Couples/Relationship therapy",
-#         "Individual therapy"
+#         "Individual therapy",
+#         "Medication management"
 #     ],
 #     "languages": ["English"],
 #     "remoteVisits": True,
@@ -24,90 +53,61 @@
 # response = requests.request("POST", url, headers=headers, data=payload)
 # print(response.text)
 
-import requests
-import json
 
-# provider request bodies 
-url = "http://heartrans-back.herokuapp.com/providers/"
-payload = json.dumps({
-    "fullName": "Chester Robachinski",
-    "otherNames": [
-        "Chet Robachinski"
-    ],
-    "titles": [
-        "MD"
-    ],
-    "specialties": "Psychiatry",
-    "services": [
-        "Couples/Relationship therapy",
-        "Individual therapy",
-        "Medication management"
-    ],
-    "languages": ["English"],
-    "remoteVisits": True,
-    "slidingScalePay": False
-})
-headers = {
-    'Content-Type': 'application/json'
-}
-response = requests.request("POST", url, headers=headers, data=payload)
-print(response.text)
+# url = "http://heartrans-back.herokuapp.com/providers/"
+# payload = json.dumps({
+#     "fullName": "Sydney Woolston",
+#     "otherNames": [
+#         "Sydney Woolston"
+#     ],
+#     "titles": [
+#         "MD"
+#     ],
+#     "specialties": ["Psychology"],
+#     "services": [
+#         "Couples/Relationship therapy",
+#         "Individual therapy",
+#     ],
+#     "languages": ["English"],
+#     "remoteVisits": True,
+#     "slidingScalePay": False
+# })
+# headers = {
+#     'Content-Type': 'application/json'
+# }
+# response = requests.request("POST", url, headers=headers, data=payload)
+# print(response.text)
 
-
-url = "http://heartrans-back.herokuapp.com/providers/"
-payload = json.dumps({
-    "fullName": "Sydney Woolston",
-    "otherNames": [
-        "Sydney Woolston"
-    ],
-    "titles": [
-        "MD"
-    ],
-    "specialties": ["Psychology"],
-    "services": [
-        "Couples/Relationship therapy",
-        "Individual therapy",
-    ],
-    "languages": ["English"],
-    "remoteVisits": True,
-    "slidingScalePay": False
-})
-headers = {
-    'Content-Type': 'application/json'
-}
-response = requests.request("POST", url, headers=headers, data=payload)
-print(response.text)
-
-url = "http://heartrans-back.herokuapp.com/providers/"
-payload = json.dumps({
-    "fullName": "Rebecca Virata",
-    "otherNames": [
-        "Rebecca Virata"
-    ],
-    "titles": [
-        "MD"
-    ],
-    "specialties": ["Primary Care"],
-    "services": [
-        "COVID-19 Testing"
-    ],
-    "languages": ["English"],
-    "remoteVisits": True,
-    "slidingScalePay": False
-})
-headers = {
-    'Content-Type': 'application/json'
-}
-response = requests.request("POST", url, headers=headers, data=payload)
-print(response.text)
+# url = "http://heartrans-back.herokuapp.com/providers/"
+# payload = json.dumps({
+#     "fullName": "Rebecca Virata",
+#     "otherNames": [
+#         "Rebecca Virata"
+#     ],
+#     "titles": [
+#         "MD"
+#     ],
+#     "specialties": ["Primary Care"],
+#     "services": [
+#         "COVID-19 Testing"
+#     ],
+#     "languages": ["English"],
+#     "remoteVisits": True,
+#     "slidingScalePay": False
+# })
+# headers = {
+#     'Content-Type': 'application/json'
+# }
+# response = requests.request("POST", url, headers=headers, data=payload)
+# print(response.text)
 
 
-# location request bodies 
-url = "http://heartrans-back.herokuapp.com/locations/new/"
-​
-payload={}
-headers = {}
-​
-response = requests.request("POST", url, headers=headers, data=payload)
-​
-print(response.text)
+# # location request bodies 
+# url = "http://heartrans-back.herokuapp.com/locations/new/"
+# ​
+# payload={}
+# headers = {}
+# ​
+# response = requests.request("POST", url, headers=headers, data=payload)
+# ​
+# print(response.text)
