973c3c252b17c8331114a2ff2391bd8b0a964c6a
Hannah Lumapas <lumapas.h@gmail.com>
2021-08-16 20:19:42 -0400

    made search bar more responsive-ish

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,19 +2,19 @@ import React, { useState } from "react";
 import styles from "./NavBar.module.css";
 import { SearchBar } from "./SearchBar";
 import { SideBar } from "./SideBar";
-import logo from '../images/heartranslogo.png'; 
+import logo from "../images/heartranslogo.png";
 
 export function NavBar() {
   return (
     <div className={styles["nav-bar"]}>
       <div className="navbar-brand">
         <a className="navbar-item" href="/">
-          <img src= {logo} alt="heartrans-logo"/>
+          <img src={logo} alt="heartrans-logo" />
         </a>
       </div>
-      <section className="navbar-menu">
+      <section className={`is-desktop ${styles["navbar-menu"]}`}>
         <div className="navbar-item">
-        <SearchBar />
+          <SearchBar />
         </div>
       </section>
       <section className={styles.right}>
