0646bf48c20089549aeb3ac79a8d14b888dbe93e
Hannah Lumapas <lumapas.h@gmail.com>
2021-08-18 12:53:56 -0400

    navbar sizing

--- src/components/Marker.tsx
+++
@@ -1,8 +0,0 @@
-// import React, { useEffect, useState } from "react";
-// import "./Marker.css";
-
-// export const Marker: React.FC<any> = ({ text }: any) => {
-//   return <div className="marker">{text}</div>;
-// };
-
-export {};
