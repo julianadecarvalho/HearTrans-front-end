95cdaf3ae050753a5c96d77e9ce24e0f81efec9d
Hannah Lumapas <lumapas.h@gmail.com>
2021-08-18 17:29:24 -0400

    updated mission

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,27 +5,40 @@ export function Home() {
     <div className="container is-primary">
       <section className="section is-section-padding-large">
         <h1 className="title is-1">Mission</h1>
-        <p className="subtitle is-3">Our mission is to help transgender, intersex, and gender nonconforming individuals find medical providers 
-        and locations. Our team hopes this app finds a way to fill the gap that exist in the community when it comes to healthcare resource for trans folks. 
-        We wanted to make sure the community is able share their knowledge and experience by leaving reviews so that others will know where to go. </p>
+        <p className="subtitle is-3">
+          Our mission is to help transgender, intersex, and gender nonconforming
+          individuals find medical providers and health resources. Our team
+          hopes to create space for trans folks to be heard and share their
+          knowledge of culturally-competent care through a community-driven
+          database and map.
+        </p>
       </section>
       <section className="section is-section-padding-large">
         <h1 className="title is-1">The HearTrans Web Application</h1>
-        <p className="subtitle is-3">On this web application you can search for locations and medical 
-        providers near you! Once you have completed your search you can click on a result you will be able see the location 
-        or provider's profile. You can even leave a review to help others know whether or not they can trust
-        that place or person. If you know of a location or provider that did not appear in the results you can 
-        add them to our database!</p>
+        <p className="subtitle is-3">
+          On this web application you can search for locations and medical
+          providers near you! Once you have completed your search you can click
+          on a result you will be able see the location or provider's profile.
+          You can even leave a review to help others know whether or not they
+          can trust that place or person. If you know of a location or provider
+          that did not appear in the results you can add them to our database!
+        </p>
       </section>
       <section className="section is-section-padding-large">
         <h1 className="title is-1">About Us</h1>
         <p className="subtitle is-3">Hi!!</p>
-        <p className="subtitle is-3">Welcome to our website! This is a capstone project created by 4 Computer Engineer Students at Ada Developers Academy. Please be gentle with our creation, but also have fun exploring it.
+        <p className="subtitle is-3">
+          Welcome to our website! This is a capstone project created by 4
+          Computer Engineer Students at Ada Developers Academy. Please be gentle
+          with our creation, but also have fun exploring it.
         </p>
       </section>
       <section className="section is-section-padding-large">
         <h1 className="title is-1">Important!</h1>
-        <p className="subtitle is-3">We don't collect user data so there are no issues about it being leaked.</p>
+        <p className="subtitle is-3">
+          We don't collect user data so there are no issues about it being
+          leaked.
+        </p>
       </section>
     </div>
   );
